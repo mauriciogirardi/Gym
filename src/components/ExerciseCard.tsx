11872c075bd98f6746c08621c6aad4a133b6270d
Mauricio Girardi <maurigirarde@yahoo.com.br>
2023-03-19 13:19:38 +0000

    Create components

---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,41 @@
+import { Heading, HStack, Icon, Image, Text, VStack } from "native-base";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+type ExerciseCardProps = TouchableOpacityProps & {};
+
+export function ExerciseCard({ ...rest }: ExerciseCardProps) {
+  return (
+    <TouchableOpacity {...rest}>
+      <HStack
+        bg="gray.500"
+        alignItems="center"
+        p={2}
+        pr={4}
+        rounded="md"
+        mb={3}
+      >
+        <Image
+          source={{ uri: "https://i.ytimg.com/vi/JE3XUqMyHXo/mqdefault.jpg" }}
+          alt="Imagem do exercício Remada unilateral"
+          w={16}
+          h={16}
+          rounded="md"
+          resizeMode="center"
+          mr={4}
+        />
+
+        <VStack flex={1}>
+          <Heading fontSize="lg" color="white">
+            Remada unilateral
+          </Heading>
+          <Text fontSize="sm" mt={1} numberOfLines={2} color="gray.200">
+            3 séries x 12 repetições
+          </Text>
+        </VStack>
+
+        <Icon as={Entypo} name="chevron-thin-right" color="gray.300" />
+      </HStack>
+    </TouchableOpacity>
+  );
+}
